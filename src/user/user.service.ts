83ca89ba6f204664d9ddc9d5e5c22f87418b543c
Max Antony <maxpacami@gmail.com>
2024-12-05 08:03:09 -0500

    feat: get users from database

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { DatabaseService } from '@/common/database/database.service';
 
 @Injectable()
 export class UserService {
+  constructor(private readonly db: DatabaseService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.db.user.findMany();
   }
 
   findOne(id: number) {
