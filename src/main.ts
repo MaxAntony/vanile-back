011f35ccee16762f760c2ecb2e89ed6ea133adad
Max Antony <maxpacami@gmail.com>
2024-12-05 08:25:26 -0500

    feat: use validation pipe globaly

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const openApiConfig = new DocumentBuilder().setTitle('E-Commerce').setDescription('Api for the E-Commerce proyect').setVersion('1.0').build();
   const documentFactory = () => SwaggerModule.createDocument(app, openApiConfig);
-
   SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
 }
