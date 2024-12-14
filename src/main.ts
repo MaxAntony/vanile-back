b419a42fcfb73bee8f3bcafe6adbb7529bb41a0a
Max Antony <maxpacami@gmail.com>
2024-12-13 20:58:12 -0500

    feat: update operation name generation for swagger, now withou "...Controller" sufix

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
+  // Swagger configs
   const openApiConfig = new DocumentBuilder().setTitle('E-Commerce').setDescription('Api for the E-Commerce proyect').setVersion('1.0').build();
-  const documentFactory = () => SwaggerModule.createDocument(app, openApiConfig);
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, openApiConfig, {
+      operationIdFactory: (controllerKey, methodKey, version) => {
+        console.log(controllerKey, methodKey, version);
+        const versionPart = version ? `_${version}` : '';
+        return `${controllerKey.replace('Controller', '')}_${methodKey}${versionPart}`;
+      },
+    });
   SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
