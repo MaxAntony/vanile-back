906e6c17d2bfdb2e8947add0cb72cb888cde96cd
Max Antony <maxpacami@gmail.com>
2025-01-22 10:20:29 -0500

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from './common/common.module';
 import { ItemModule } from './item/item.module';
 import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, CommonModule, ItemModule, OrderModule],
+  imports: [UserModule, CommonModule, ItemModule, OrderModule, AuthModule],
 })
 export class AppModule {}
